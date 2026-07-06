2e2b6a5b9f6fb15f936c5e2c8433b3f3b18c8682
Timothy <46894223+timothy692@users.noreply.github.com>
2026-07-07 01:48:44 +0200

    Add Krea2 model downloads and update commands
    Added new model downloads for Krea2 including raw int8 and turbo LoRa models. Updated existing download commands and commented out unused lines.

--- krea2.sh
+++ krea2.sh
@@ -71,21 +71,23 @@ dl_civitai() {
 dl_from "Comfy-Org/Krea-2" "text_encoders/qwen3vl_4b_fp8_scaled.safetensors" "${COMFY_DIR}/text_encoders"
 dl_from "Comfy-Org/Krea-2" "vae/qwen_image_vae.safetensors" "${COMFY_DIR}/vae"
 
+# Krea2 raw int8 (with turbo LoRa)
+dl_from "Comfy-Org/Krea-2" "diffusion_models/krea2_raw_int8_convrot.safetensors" "${COMFY_DIR}/diffusion_models"
+dl_from "Comfy-Org/Krea-2" "loras/krea2_turbo_lora_rank_64_bf16.safetensors" "${COMFY_DIR}/loras"
 # Turbo fp8
-dl_from "Comfy-Org/Krea-2" "diffusion_models/krea2_turbo_fp8_scaled.safetensors" "${COMFY_DIR}/diffusion_models"
-
-# Turbo bf16
-# dl_from "Comfy-Org/Krea-2" "diffusion_models/krea2_turbo_bf16.safetensors" "${COMFY_DIR}/diffusion_models"
-
+# dl_from "Comfy-Org/Krea-2" "diffusion_models/krea2_turbo_fp8_scaled.safetensors" "${COMFY_DIR}/diffusion_models"
 # Redcraft
 # dl_civitai "https://civitai.com/api/download/models/3066243?fileId=2945029" "redcraftKREA2RedMix_krea2Edition.safetensors" "${COMFY_DIR}/diffusion_models"
 
 # LoRas
 dl_civitai "https://civitai.red/api/download/models/3070702?fileId=2949534" "realism_engine_krea2_v2.safetensors" "${COMFY_DIR}/loras"
 dl_civitai "https://civitai.red/api/download/models/3067151?fileId=2945865" "krea2_filterbypass_3vec.safetensors" "${COMFY_DIR}/loras"
+dl_civitai "https://civitai.red/api/download/models/3104629?fileId=2984442" "snofs_krea_v1_1.safetensors" "${COMFY_DIR}/loras"
+dl_civitai "https://civitai.red/api/download/models/3075606?fileId=2954661" "lenovo_krea2.safetensors" "${COMFY_DIR}/loras"
+
+dl_from "timothy692/h4na_v1" "h4na_v1_krea2_lora_000002500.safetensors" "${COMFY_DIR}/loras"
 dl_from "timothy692/h4na_v1" "h4na_v1_krea2_lora_000003000.safetensors" "${COMFY_DIR}/loras"
 dl_from "timothy692/h4na_v1" "h4na_v1_krea2_lora_000003750.safetensors" "${COMFY_DIR}/loras"
 
-# dl_civitai "https://civitai.red/api/download/models/3075606?fileId=2954661" "lenovo_krea2.safetensors" "${COMFY_DIR}/loras"
 # dl_civitai "https://civitai.red/api/download/models/3075498?fileId=2954554" "nicegirls_krea2.safetensors" "${COMFY_DIR}/loras"
 # dl_civitai "https://civitai.red/api/download/models/3084537?fileId=2963911" "realisticsnapshot_krea2.safetensors" "${COMFY_DIR}/loras"
