2c07f5e6081f78ec0f2a198f83dc76314faab41c
Timothy <46894223+timothy692@users.noreply.github.com>
2026-07-19 18:56:54 +0200

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -73,5 +73,6 @@ run_module() {
 # run_module "zimageturbo.sh"
 # run_module "sdxl_zimageturbo.sh"
 # run_module "post_processing.sh"
-run_module "krea2.sh"
+# run_module "krea2.sh"
 #run_module "synthid_bypass.sh"
+run_module "bg_flux_kontent.sh"
