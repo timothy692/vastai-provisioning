0aa30392a889b4ee76badab7325246ef5c8ad45f
Timothy <46894223+timothy692@users.noreply.github.com>
2026-07-19 12:08:17 +0200

    Update post_processing.sh

--- post_processing.sh
+++ post_processing.sh
@@ -66,7 +66,9 @@ dl_civitai() {
 
 dl_from "titomatus0203/qwen_3_8b_fp8mixed" "qwen_3_8b_fp8mixed.safetensors" "${COMFY_DIR}/text_encoders"
 # dl_from "black-forest-labs/FLUX.2-klein-9b-fp8" "flux-2-klein-9b-fp8.safetensors" "${COMFY_DIR}/diffusion_models"
+
 dl_civitai "https://civitai.red/api/download/models/2973304?fileId=2852910" "pornmaster_fluxklein9b.safetensors" "${COMFY_DIR}/diffusion_models"
+dl_civitai "https://civitai.red/api/download/models/2960556?fileId=2839878" "snofs_fluxklein9b_v1_4.safetensors" "${COMFY_DIR}/loras"
 
 dl "flux2-vae.safetensors" "${COMFY_DIR}/vae"
 
