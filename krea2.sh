3795bb0fc06150ac8e8e7eb6cf8fc0f3553c8737
Timothy <46894223+timothy692@users.noreply.github.com>
2026-07-19 12:56:24 +0200

    Update krea2.sh

--- krea2.sh
+++ krea2.sh
@@ -78,10 +78,10 @@ dl_from "wangkanai/wan21-vae" "vae/wan/wan21-vae.safetensors" "${COMFY_DIR}/vae"
 # dl_from "Comfy-Org/Krea-2" "vae/qwen_image_vae.safetensors" "${COMFY_DIR}/vae"
 
 # Krea2 raw int8 (with turbo LoRa)
-dl_from "Comfy-Org/Krea-2" "diffusion_models/krea2_raw_int8_convrot.safetensors" "${COMFY_DIR}/diffusion_models"
-dl_from "Comfy-Org/Krea-2" "loras/krea2_turbo_lora_rank_64_bf16.safetensors" "${COMFY_DIR}/loras"
+# dl_from "Comfy-Org/Krea-2" "diffusion_models/krea2_raw_int8_convrot.safetensors" "${COMFY_DIR}/diffusion_models"
+# dl_from "Comfy-Org/Krea-2" "loras/krea2_turbo_lora_rank_64_bf16.safetensors" "${COMFY_DIR}/loras"
 # Turbo fp8
-# dl_from "Comfy-Org/Krea-2" "diffusion_models/krea2_turbo_fp8_scaled.safetensors" "${COMFY_DIR}/diffusion_models"
+dl_from "Comfy-Org/Krea-2" "diffusion_models/diffusion_models/krea2_turbo_int8_convrot.safetensors" "${COMFY_DIR}/diffusion_models"
 # Redcraft
 # dl_civitai "https://civitai.com/api/download/models/3066243?fileId=2945029" "redcraftKREA2RedMix_krea2Edition.safetensors" "${COMFY_DIR}/diffusion_models"
 
@@ -91,9 +91,12 @@ dl_civitai "https://civitai.red/api/download/models/3067151?fileId=2945865" "kre
 dl_civitai "https://civitai.red/api/download/models/3104629?fileId=2984442" "snofs_krea_v1_1.safetensors" "${COMFY_DIR}/loras"
 dl_civitai "https://civitai.red/api/download/models/3075606?fileId=2954661" "lenovo_krea2.safetensors" "${COMFY_DIR}/loras"
 
-dl_from "timothy692/h4na_v1" "h4na_v1_krea2_lora_000002500.safetensors" "${COMFY_DIR}/loras"
-dl_from "timothy692/h4na_v1" "h4na_v1_krea2_lora_000003000.safetensors" "${COMFY_DIR}/loras"
-dl_from "timothy692/h4na_v1" "h4na_v1_krea2_lora_000003750.safetensors" "${COMFY_DIR}/loras"
+# Krea2 EDIT
+dl_from "conradlocke/krea2-identity-edit" "krea2_identity_edit_v1_2.safetensors" "${COMFY_DIR}/diffusion_models"
+
+# dl_from "timothy692/h4na_v1" "h4na_v1_krea2_lora_000002500.safetensors" "${COMFY_DIR}/loras"
+# dl_from "timothy692/h4na_v1" "h4na_v1_krea2_lora_000003000.safetensors" "${COMFY_DIR}/loras"
+# dl_from "timothy692/h4na_v1" "h4na_v1_krea2_lora_000003750.safetensors" "${COMFY_DIR}/loras"
 
 # dl_civitai "https://civitai.red/api/download/models/3075498?fileId=2954554" "nicegirls_krea2.safetensors" "${COMFY_DIR}/loras"
 # dl_civitai "https://civitai.red/api/download/models/3084537?fileId=2963911" "realisticsnapshot_krea2.safetensors" "${COMFY_DIR}/loras"
