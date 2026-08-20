05debcb76792ca83b15fee1fd797d0a15a3ce986
Timothy <46894223+timothy692@users.noreply.github.com>
2026-08-20 20:14:34 +0200

    Update krea2.sh

--- krea2.sh
+++ krea2.sh
@@ -94,8 +94,8 @@ dl_civitai "https://civitai.red/api/download/models/3067151?fileId=2945865" "kre
 dl_civitai "https://civitai.red/api/download/models/3104629?fileId=2984442" "snofs_krea_v1_1.safetensors" "${COMFY_DIR}/loras"
 dl_civitai "https://civitai.red/api/download/models/3075606?fileId=2954661" "lenovo_krea2.safetensors" "${COMFY_DIR}/loras"
 dl_civitai "https://civitai.com/api/download/models/3151907?fileId=3032561" "krea-smartphone-photo-slider.safetensors" "${COMFY_DIR}/loras"
+dl_civitai "https://civitai.red/api/download/models/3106092?fileId=2985983" "PornMaster_Low_Resolution_Slider_Krea2_V1.safetensors" "${COMFY_DIR}/loras"
 
-# dl_civitai "https://civitai.red/api/download/models/3090634?fileId=2970148" "Krea2-realism-V2.safetensors" "${COMFY_DIR}/loras"
 # dl_civitai "https://civitai.com/api/download/models/3123277?fileId=3003694" "phone_photography_2025_krea2.safetensors" "${COMFY_DIR}/loras"
 
 # dl_civitai "https://civitai.red/api/download/models/3097834?fileId=2977474" "skindetails_krea2_loraholic.safetensors" "${COMFY_DIR}/loras"
