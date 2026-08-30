40bd9676ef82601b1e72531232c8287d9270f4ee
Timothy <46894223+timothy692@users.noreply.github.com>
2026-08-30 20:36:34 +0200

    Update krea2.sh

--- krea2.sh
+++ krea2.sh
@@ -90,7 +90,7 @@ dl_from "Comfy-Org/Krea-2" "loras/krea2_turbo_lora_rank_64_bf16.safetensors" "${
 
 # LoRas
 dl_civitai "https://civitai.red/api/download/models/3109006?fileId=2988982" "realism_engine_krea2_v3.safetensors" "${COMFY_DIR}/loras"
-dl_civitai "https://civitai.red/api/download/models/3067151?fileId=2945865" "krea2_filterbypass_3vec.safetensors" "${COMFY_DIR}/loras"
+# dl_civitai "https://civitai.red/api/download/models/3067151?fileId=2945865" "krea2_filterbypass_3vec.safetensors" "${COMFY_DIR}/loras"
 dl_civitai "https://civitai.red/api/download/models/3104629?fileId=2984442" "snofs_krea_v1_1.safetensors" "${COMFY_DIR}/loras"
 dl_civitai "https://civitai.red/api/download/models/3075606?fileId=2954661" "lenovo_krea2.safetensors" "${COMFY_DIR}/loras"
 dl_civitai "https://civitai.com/api/download/models/3151907?fileId=3032561" "krea-smartphone-photo-slider.safetensors" "${COMFY_DIR}/loras"
